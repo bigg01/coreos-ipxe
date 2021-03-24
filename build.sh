ec15277d1e461fd9dae94d13f0198a38837c0616
Oliver Guggenbuehl <oliver.guggenbuehl@six-group.com>
2021-03-24 15:58:05 +0100

    ini

--- build.sh
+++ build.sh
@@ -1,5 +1,11 @@
 IMAGENAME=ocp-coreos-ipxe
 VERSION=v1
-docker build  -t bigg01/${IMAGENAME}:${VERSION} .
+alias docker="podman"
+docker build  -t quay.io/bigg01/coreos-ipxe/${IMAGENAME}:${VERSION} .
 #docker build --no-cache -t bigg01/${IMAGENAME}:${VERSION} .
-#docker push bigg01/${IMAGENAME}:${VERSION}
+docker push quay.io/bigg01/coreos-ipxe/${IMAGENAME}:${VERSION}
+
+
+docker build  -t quay.io/bigg01/coreos-ipxe/${IMAGENAME}:${VERSION} .
+#docker build --no-cache -t bigg01/${IMAGENAME}:${VERSION} .
+docker push quay.io/bigg01/coreos-ipxe/${IMAGENAME}:${VERSION}
